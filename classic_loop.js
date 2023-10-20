fc6b7376f0f36467cb7bece8408c2617e1c857ed
MarcelDurgante <marceldur@gmail.com>
2023-10-20 06:15:20 +0200

    Adjusted comments

--- classic_loop.js
+++ classic_loop.js
@@ -3,10 +3,27 @@ for(let step = 0; step <3; step++) {
     console.log(`Walking ${step} step`);
 }
 
+/* Output
+
+Walking 0 step
+Walking 1 step
+Walking 2 step
+
+*/
+
 console.log('Backwards');
 for(let step = 3; step > 0; step--) {
     console.log(`Walking ${step} step`);
 }
+
+/* Output
+
+Backwards
+Walking 3 step
+Walking 2 step
+Walking 1 step
+
+*/
 
 // iterate up to the middle, work on the condiction section. Increment/decrement on every second or third etc element, do it on the iterator section
 console.log('Getting every nth element up to the middle of the array');
@@ -15,22 +32,41 @@ for(let i = 0; i < (array.length / 2); i+=2) {
     console.log(`Every second element: ${i}`);
 }
 
-
-/* Classic loop
-
-Walking 0 step
-Walking 1 step
-Walking 2 step
-
-
-Backwards
-Walking 3 step
-Walking 2 step
-Walking 1 step
+/* Output
 
 
 Getting every nth element up to the middle of the array
 
 Every second element: 0
 Every second element: 2
-Every second element: 4 */
+Every second element: 4 
+
+*/
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+/* Output
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+*/
+
+
+
+
+
+
+
+
